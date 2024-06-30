827ea300e4d33d470fb9e05301fe0104742ea50c
Joel Koch <joel@joelkoch.dev>
2024-06-30 15:39:46 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # next-ls = {
+    #   url = "github:elixir-tools/next-ls";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    # ghostty = {
+    #   url = "git+ssh://git@github.com/ghostty-org/ghostty";
+    # };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    # next-ls,
+    # ghostty,
+    ...
+  } @ attrs: {
+    nixosConfigurations.pine = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      specialArgs = attrs;
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.joel = import ./home.nix;
+          specialArgs = attrs;
+        }
+      ];
+    };
+
+    homeConfigurations."joel" = home-manager.lib.homeManagerConfiguration {
+      pkgs = {inherit nixpkgs;}.legacyPackages.x86_64-linux;
+
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [./home.nix];
+
+      # Optionally use extraSpecialArgs
+      # to pass through arguments to home.nix
+      # extraSpecialArgs = {inherit next-ls;};
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,186 @@
+{
+  config,
+  pkgs,
+  # next-ls,
+  # ghostty,
+  ...
+}: {
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "joel";
+  home.homeDirectory = "/home/joel";
+
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  programs = {
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting # Disable greeting
+      '';
+      plugins = [
+        {
+          name = "tide";
+          src = pkgs.fishPlugins.tide.src;
+        }
+        {
+          name = "plugin-git";
+          src = pkgs.fishPlugins.plugin-git.src;
+        }
+      ];
+    };
+
+    bash = {
+      enable = true;
+      initExtra = ''
+        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+        then
+          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+        fi
+      '';
+    };
+
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      nix-direnv.enable = true;
+    };
+
+    git = {
+      enable = true;
+      userName = "Joel Koch";
+      userEmail = "joel@joelkoch.dev";
+    };
+
+    helix = {
+      enable = true;
+      defaultEditor = true;
+      settings = {
+        theme = "onedark";
+        editor = {
+          auto-save = true;
+          line-number = "relative";
+          file-picker.hidden = false;
+          lsp.display-messages = true;
+        };
+      };
+      languages = {
+        language-server = {
+          nextls = {
+            command = "nextls";
+            args = ["--stdio=true"];
+            config = {experimental = {completions = {enable = true;};};};
+            environment = {"NEXTLS_SPITFIRE_ENABLED" = "1";};
+          };
+
+          tailwind-heex = {
+            command = "tailwindcss-language-server";
+            args = ["--stdio"];
+          };
+          nil = {
+            command = "nil";
+            config = {nil.formatting = {command = ["alejandra"];};};
+          };
+        };
+
+        language = [
+          {
+            name = "nix";
+            language-servers = ["nil"];
+            auto-format = true;
+          }
+          {
+            name = "elixir";
+            language-servers = ["nextls"];
+            auto-format = true;
+            roots = ["mix.exs"];
+            scope = "source.elixir";
+          }
+          {
+            name = "heex";
+            language-servers = ["nextls"];
+            auto-format = true;
+          }
+        ];
+      };
+    };
+
+    zellij = {
+      enable = true;
+    };
+
+    alacritty = {
+      enable = true;
+      settings.shell = {
+        program = "zellij";
+        args = ["-l" "welcome"];
+      };
+    };
+  };
+
+  home.packages = with pkgs;
+    [
+      neofetch
+      fzf
+      lazygit
+      gitui
+
+      act
+      alejandra
+      asciinema
+      bat
+      bottom
+      gh
+      htop
+      jq
+      ncdu
+      nil
+      ripgrep
+      tokei
+      tree
+      xclip
+      xh
+    ]
+    ++ [
+      # next-ls.packages.${pkgs.system}.default
+      # ghostty.packages.${pkgs.system}.default
+    ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  home.sessionVariables = {
+    EDITOR = "hx";
+    VISUAL = "hx";
+  };
+
+  home.shellAliases = {
+    l = "ls -lAh1";
+
+    hm = "home-manager";
+
+    hmup = "nix flake update ~/hm";
+    hmsw = "home-manager switch --flake ~/hm";
+
+    lg = "lazygit";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
